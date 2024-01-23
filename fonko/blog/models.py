192271dc8c1d85f7813058dd57ad9f925c52eb96
Ondrej <panlux789@gmail.com>
2024-01-23 09:35:10 +0100

    BRUH

--- fonko/blog/models.py
+++ fonko/blog/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class brug(models.Model):
-    nahodne_policko = models.CharField
+    nahodne_policko = models.CharField(max_length=200, default="x")
+    title = models.CharField(max_length=200,default="x")
+
