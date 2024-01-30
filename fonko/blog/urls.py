6b8ce48ea17757a21f5fa4a85f32a3f3b31a882f
Ondrej <panlux789@gmail.com>
2024-01-30 09:18:59 +0100

    BRUH

--- fonko/blog/urls.py
+++ fonko/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = {
-    path("", views.models_list)
+    path("", views.models_List)
 }
--- fonko/blog/views.py
+++ fonko/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def models_List(request):
-    return HttpResponse("ahoj")
+    random_number = random.randint(a=1,b=100)
+    return HttpResponse(f"Nahodne cislo {random_number}")
+
+
